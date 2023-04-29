50db646204a1ec91a7be910b2d15cdeb43996480
Sreerag M <sreeragibtl@gmail.com>
2023-04-29 15:10:55 +0530

    test: Assert create_audio_base_64

--- tests/test_equality.py
+++ tests/test_equality.py
@@ -7,7 +7,7 @@ Usage:
 
 """
 
-from base64 import b64decode
+from base64 import b64decode, b64encode
 from itertools import repeat
 from os.path import exists
 from typing import Any, Callable, List
@@ -17,6 +17,7 @@ import pytest
 from fastapi import Response
 from fastapi.testclient import TestClient
 
+from api.create_audio_base_64 import create_audio_base_64
 from api.create_unique_filename import create_unique_filename
 from api.prediction import app
 from api.remove_file import remove_file
@@ -48,6 +49,22 @@ open("empty_file", "wb").close()
 # Remove file.
 remove_file("empty_file")
 
+# Create file for testing create_audio_base_64.
+file_for_testing_create_audio_base_64 = open(
+    "file_for_testing_create_audio_base_64"
+)  # ...........
+
+# Convert to base64.
+base_64: str = create_audio_base_64("empty_file")
+
+# Decode and encode back.
+decode_encode_audio_base_64: str = b64encode(b64decode(base_64)).decode(
+    "utf-8"
+)  # ................
+
+# Remove file.
+remove_file("file_for_testing_create_audio_base_64")
+
 
 @pytest.mark.parametrize(
     "lhs, rhs, assert_equality",
@@ -56,6 +73,7 @@ remove_file("empty_file")
         (decoded_audio_has_riff, True, test_case.assertEqual),
         (len(set(unique_filenames)), 100, test_case.assertEqual),
         (exists("empty_file"), False, test_case.assertEqual),
+        (decode_encode_audio_base_64, audio_base_64, test_case.assertEqual),
     ),  # ............
 )
 def test_equality(
