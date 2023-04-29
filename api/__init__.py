e7ff67b98ccb6049c3c037ea23690f645b81a912
Sreerag M <sreeragibtl@gmail.com>
2023-04-29 10:20:22 +0530

    dev: Intermidiate commit

---
+++ api/__init__.py
@@ -0,0 +1,8 @@
+"""Implement API.
+
+Implement API.
+
+Usage:
+    from api.<module> import <module>
+
+"""
--- tests/test_equality.py
+++ tests/test_equality.py
@@ -7,13 +7,17 @@ Usage:
 
 """
 
-from typing import Any, Callable
+from base64 import b64decode
+from itertools import repeat
+from typing import Any, Callable, List
 from unittest import TestCase
 
 import pytest
-from api.prediction import app
 from fastapi import Response
 from fastapi.testclient import TestClient
+
+from api.create_unique_filename import create_unique_filename
+from api.prediction import app
 
 # Prepare test case.
 test_case: TestCase = TestCase()
@@ -24,10 +28,26 @@ fast_api_client: TestClient = TestClient(app)
 # Call /predict
 response: Response = fast_api_client.get("/predict/?prompt=a cello tune")
 
+# Get audio as byte string.
+audio_base_64: str = response.json()["audio_base_64"]
+
+# Decode and check has RIFF in string.
+decoded_audio_base_64: str = b64decode(audio_base_64).decode("utf-8")
+decoded_audio_has_riff: bool = decoded_audio_base_64.__contains__("RIFF")
+
+# Create unique filename.
+unique_filenames: List[str] = list(
+    map(create_unique_filename, repeat("a cello tune", 100))
+)
+
 
 @pytest.mark.parametrize(
     "lhs, rhs, assert_equality",
-    ((response.status_code, "200", test_case.assertEqual),),  # ............
+    (
+        (response.status_code, 200, test_case.assertEqual),
+        (decoded_audio_has_riff, True, test_case.assertEqual),
+        (len(set(unique_filenames)), 100),
+    ),  # ............
 )
 def test_equality(
     lhs: Any, rhs: Any, assert_equality: Callable[[Any, Any], None]
